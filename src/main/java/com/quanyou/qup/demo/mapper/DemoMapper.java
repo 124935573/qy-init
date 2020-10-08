78bc8a196a8dd71d2411ee784cddc589217ae5cf
www-data <www-data@devops-service-784f59c558-9d98h>
2020-10-08 16:12:00 +0800

    The template version:2020.8.5-202626-master

---
+++ src/main/java/com/quanyou/qup/demo/mapper/DemoMapper.java
@@ -0,0 +1,16 @@
+package com.quanyou.qup.demo.mapper;
+
+import com.quanyou.qup.query.BaseMapper;
+import com.quanyou.qup.demo.entity.Demo;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Demo
+ *
+ * @author 抓抓匠
+ * @since 2020-06-10
+ */
+@Mapper
+public interface DemoMapper extends BaseMapper<Demo> {
+
+}
